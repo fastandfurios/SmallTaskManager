c158effdf89377459aa90f84d407f992b0f4fc95
dm <semu-dima@yandex.ru>
2021-06-05 23:39:28 +0300

    adds controllers

---
+++ MetricsManager/Agents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsManager
+{
+    public sealed class Agents
+    {
+	    public List<AgentInfo> ListAgents;
+
+	    public Agents() => ListAgents = new List<AgentInfo>();
+    }
+}
---
+++ SmallTaskManager/Controllers/HddMetricsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HddMetricsController : ControllerBase
+	{
+		[HttpGet("hdd/left")]
+		public IActionResult GetMetricsFromAgent([FromRoute] int freeSpace)
+		{
+			return Ok();
+		}
+	}
+}
