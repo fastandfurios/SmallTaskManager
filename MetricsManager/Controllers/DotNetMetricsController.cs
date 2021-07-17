3d7c55e195276299fbc4a38aece447bb335d494d
Дмитрий <semu-dima@yandex.ru>
2021-07-17 11:12:46 +0300

    :bookmark: added comments to the other controllers

--- MetricsManager/Controllers/DotNetMetricsController.cs
+++ MetricsManager/Controllers/DotNetMetricsController.cs
@@ -29,6 +29,20 @@ namespace MetricsManager.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Получает метрики .NET на заданном диапазоне времени и номер агента, который эти метрики собрал
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса:
+        ///
+        ///     GET agent/1/from/1970.01.01 00:00:00/to/1970.01.01 00:10:00
+        ///
+        /// </remarks>
+        /// <param name="fromTime">начальная метрка времени</param>
+        /// <param name="toTime">конечная метрка времени</param>
+        /// <returns>Список метрик, которые были сохранены в заданном диапазоне времени</returns>
+        /// <response code="200">если все хорошо</response>
+        /// <response code="400">если передали не правильные параметры</response>
 		[HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
 		public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
 		{
@@ -49,6 +63,20 @@ namespace MetricsManager.Controllers
 			return Ok(response);
 		}
 
+        /// <summary>
+        /// Получает метрики .NET на заданном диапазоне времени
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса:
+        ///
+        ///     GET agent/1/from/1970.01.01 00:00:00/to/1970.01.01 00:10:00
+        ///
+        /// </remarks>
+        /// <param name="fromTime">начальная метрка времени</param>
+        /// <param name="toTime">конечная метрка времени</param>
+        /// <returns>Список метрик, которые были сохранены в заданном диапазоне времени</returns>
+        /// <response code="200">если все хорошо</response>
+        /// <response code="400">если передали не правильные параметры</response>
 		[HttpGet("cluster/from/{fromTime}/to/{toTime}")]
 		public IActionResult GetMetricsFromAllCluster([FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
 		{
