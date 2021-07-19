d22726b472735c0f5e946b837d727c8e9faf94ac
Дмитрий <semu-dima@yandex.ru>
2021-07-19 21:26:33 +0300

    :construction_worker: added DI

--- MetricsManagerClient/App.xaml.cs
+++ MetricsManagerClient/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MetricsManagerClient.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MetricsManagerClient
 {
@@ -13,5 +15,25 @@ namespace MetricsManagerClient
     /// </summary>
     public partial class App : Application
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
     }
 }
