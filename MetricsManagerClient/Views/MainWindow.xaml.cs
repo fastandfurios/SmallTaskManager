f89a30f55658f712dd9791b242a3598ed7f51d4f
Дмитрий <semu-dima@yandex.ru>
2021-07-19 16:16:50 +0300

    :construction: added mvvm

--- MetricsManagerClient/Views/MainWindow.xaml.cs
+++ MetricsManagerClient/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MetricsManagerClient
+namespace MetricsManagerClient.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
